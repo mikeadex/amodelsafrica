f4aee43e41927aea3e94d08c7d8273a27031fd68
Michael Adeleye <joko.adeleye@gmail.com>
2019-08-09 14:51:12 +0100

    projects, blog and co

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_09_005454) do
+ActiveRecord::Schema.define(version: 2019_08_09_131711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,28 @@ ActiveRecord::Schema.define(version: 2019_08_09_005454) do
     t.string "ig"
     t.string "email"
     t.string "tel"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.text "shortdesc"
+    t.text "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.string "category"
+    t.text "description"
+    t.string "pin"
+    t.string "linkedin"
+    t.string "ig"
+    t.string "fb"
+    t.string "tw"
+    t.string "youtube"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
